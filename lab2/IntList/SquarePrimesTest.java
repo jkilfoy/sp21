bd72ad3e056397d1b4361ce98c27fb1eb4675e0e
jkilfoy <jordankilfoy@gmail.com>
2022-02-07 11:01:57 -0500

    completed lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -3,6 +3,9 @@ package IntList;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SquarePrimesTest {
 
     /**
@@ -16,5 +19,27 @@ public class SquarePrimesTest {
         boolean changed = IntListExercises.squarePrimes(lst);
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesTo100() {
+        IntList lst = IntList.of(1);
+        List<Integer> squarePrimes = new ArrayList<>();
+        squarePrimes.add(1);
+        for (int i = 2; i < 1000; i++) {
+            if (Primes.isPrime(i)) {
+                squarePrimes.add(0, i*i);
+            } else {
+                squarePrimes.add(0, i);
+            }
+            lst = new IntList(i, lst);
+        }
+        boolean changed = IntListExercises.squarePrimes(lst);
+        for (int i = 0; i < 999; i++) {
+            System.out.println("lst at i: " + lst.get(i) + " -- squarePrimes at i" + squarePrimes.get(i));
+            assert lst.get(i) == squarePrimes.get(i);
+        }
+        assertTrue(changed);
+
     }
 }
